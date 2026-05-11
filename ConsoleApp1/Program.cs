03a1cf2f5df9ae7d3ca07e787105e7b2c6442210
mazen <mazen@MAZENMANSOUR19>
2026-05-11 15:20:59 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Specialized;
+using System.ComponentModel.Design;
+using System.Diagnostics;
+using System.Diagnostics.Metrics;
+using System.Runtime.InteropServices;
+using System.Text;
+
+internal class Program1
+{
+    static void Main(string[] args)
+    {
+        
+            Console.Write("Enter a day number (1-7): ");
+
+        bool isParsed = int.TryParse(Console.ReadLine(), out int dayNumber);
+        if(isParsed && dayNumber>=1 && dayNumber <=7 )
+            {
+            DayOfWeek day = (DayOfWeek)dayNumber;
+           
+
+                Console.WriteLine("Day: " + day);
+
+                switch (day)
+                {
+                    case DayOfWeek.Friday:
+                    case DayOfWeek.Saturday:
+                        Console.WriteLine("Weekend");
+                        break;
+
+                    default:
+                        Console.WriteLine("Workday");
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid input!");
+            }
+        }
+    }
+        
---
+++ Enum.cs
@@ -0,0 +1,12 @@
+﻿using System;
+public enum DaysOfWeek
+{
+    Saturday,
+    Sunday,
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday
+}
+
